70c287e461ee02fbf1f605d80403b0d6ce8cb6e7
NBAProjectC <nbaprojectc@nxk.io>
2019-05-22 04:17:48 +0000

    site update

---
+++ precache-manifest.a29a7fa2ae1226a86ed7dcf0594f3f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f1ddc5329233a88d200",
+    "url": "/NbaProjectCQA/static/js/runtime~main.1003313a.js"
+  },
+  {
+    "revision": "98dc677b4cdc9a425b23",
+    "url": "/NbaProjectCQA/static/js/main.c336acdd.chunk.js"
+  },
+  {
+    "revision": "1e616caa45fc0d5e924e",
+    "url": "/NbaProjectCQA/static/js/2.25f1f484.chunk.js"
+  },
+  {
+    "revision": "98dc677b4cdc9a425b23",
+    "url": "/NbaProjectCQA/static/css/main.18b592e3.chunk.css"
+  },
+  {
+    "revision": "1e616caa45fc0d5e924e",
+    "url": "/NbaProjectCQA/static/css/2.f869753d.chunk.css"
+  },
+  {
+    "revision": "bd7ab2d46b31f7dd28ae7d1bedd211c6",
+    "url": "/NbaProjectCQA/index.html"
+  }
+];
